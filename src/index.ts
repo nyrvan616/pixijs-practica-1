f96f9aa5cc1ff7ca70ed1342482916681f8529fa
Nyrvan <fernandezr.nicolas@gmail.com>
2024-03-11 18:55:28 -0300

    Clase 1 - Video 1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js';
 
-const app = new Application<HTMLCanvasElement>({
+const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
@@ -9,11 +9,20 @@ const app = new Application<HTMLCanvasElement>({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+Loader.shared.add({url: "./clampy.png", name: "Clampy"});
+Loader.shared.add({url: "./link.png", name: "Link"});
 
-clampy.anchor.set(0.5);
+Loader.shared.onComplete.add(()=>{
+	const clampy: Sprite = Sprite.from("Link");
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+	console.log("Hola mundo!", clampy.width, clampy.height)
 
-app.stage.addChild(clampy);
+	clampy.anchor.set(0.5);
+	
+	clampy.x = app.screen.width / 2;
+	clampy.y = app.screen.height / 2;
+	
+	app.stage.addChild(clampy);
+});
+
+Loader.shared.load();
