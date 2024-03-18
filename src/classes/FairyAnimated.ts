071336802f720a74ae58dd9c26358329ef4e5b6a
Nyrvan <fernandezr.nicolas@gmail.com>
2024-03-18 20:38:12 -0300

    Clase 5: agregué movimientos básicos

--- src/classes/FairyAnimated.ts
+++ src/classes/FairyAnimated.ts
@@ -1,11 +1,13 @@
-import { AnimatedSprite, Container, Texture } from "pixi.js";
+import { AnimatedSprite, Container, Texture, Ticker } from "pixi.js";
+import { Keyboard } from "../utils/Keyboard";
 
 export class FairyAnimated extends Container {
+    private fairyAnimated: AnimatedSprite;
 
     constructor(){
         super();
 
-        const fairyAnimated: AnimatedSprite = new AnimatedSprite(
+        this.fairyAnimated = new AnimatedSprite(
             [
                 Texture.from("FairyFlaying1"),
                 Texture.from("FairyFlaying2"),
@@ -16,11 +18,33 @@ export class FairyAnimated extends Container {
                 Texture.from("FairyFlaying7"),
                 Texture.from("FairyFlaying8"),
             ],
-            true
+            false
         );
-        fairyAnimated.position.set(700,200);
-        fairyAnimated.play();
-        fairyAnimated.animationSpeed = 0.4;
-        this.addChild(fairyAnimated);
-    }  
+        this.fairyAnimated.position.set(700,200);
+        this.fairyAnimated.play();
+        this.fairyAnimated.animationSpeed = 0.4;
+        this.addChild(this.fairyAnimated);
+
+        Ticker.shared.add(this.update, this);
+    }
+
+    private update(deltaFrame:number){
+        this.fairyAnimated.update(deltaFrame);
+
+        if (Keyboard.state.get("KeyD")){
+            this.fairyAnimated.x++;
+        };
+
+        if (Keyboard.state.get("KeyA")){
+            this.fairyAnimated.x--;
+        };
+
+        if (Keyboard.state.get("KeyW")){
+            this.fairyAnimated.y--;
+        };
+
+        if (Keyboard.state.get("KeyS")){
+            this.fairyAnimated.y++;
+        };
+    }
 };
---
+++ src/utils/SceneBase.ts
@@ -0,0 +1,7 @@
+import { Container } from "pixi.js";
+
+export class SceneBase extends Container{
+    public update(_deltaTime:number, _deltaFrame?:number){
+
+    }
+}
